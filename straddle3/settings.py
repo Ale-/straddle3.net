042dd42ecbe909e22aeb6da2e2c9bbcba595c299
ale <a@414c45.net>
2018-03-26 17:13:27 +0200

    fix: Add locale middleware

--- straddle3/settings.py
+++ straddle3/settings.py
@@ -22,7 +22,6 @@ LOGOUT_URL = '/logout'
 # Name of site in the document title
 DOCUMENT_TITLE = 'straddle3'
 
-
 # Application definition
 
 CONTRIB_APPS = [
@@ -50,6 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',

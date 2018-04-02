373faf73697b989a715550edb76fced61e9e1afc
ale <a@414c45.net>
2018-04-02 16:34:44 +0200

    fix: Update API map calls

--- apps/views/views.py
+++ apps/views/views.py
@@ -23,32 +23,34 @@ class MapView(View):
 # Dataset fake API for testing D3 widgets
 def MapApi(request):
     markers = []
-    projects    = models.Project.objects.all()
+    projects    = models.Project.objects.filter(geolocation__isnull=False)
     for project in projects:
         img = project.images.first()
         start_date = project.start_date
         end_date   = project.end_date
+        category   = project.category
         markers.append({
             'name'       : project.name,
             'slug'       : project.slug,
             'pos'        : project.geolocation,
-            'cat'        : project.category.name,
-            'col'        : project.category.color,
+            'cat'        : category.name if category else None,
+            'col'        : category.color if category else None,
             'img'        : img.image_file.url if img else None,
             'start_date' : start_date.strftime("%d %b %Y") if start_date else None,
             'end_date'   : end_date.strftime("%d %b %Y") if end_date else None,
         })
-    connections = models.Connection.objects.all()
+    connections = models.Connection.objects.filter(geolocation__isnull=False)
     for connection in connections:
         img = connection.images.first()
         start_date = connection.start_date
         end_date   = connection.end_date
+        category   = connection.category
         markers.append({
             'name'       : connection.name,
             'slug'       : connection.slug,
             'pos'        : connection.geolocation,
-            'cat'        : connection.category.name,
-            'col'        : connection.category.color,
+            'cat'        : category.name if category else None,
+            'col'        : category.color if category else None,
             'img'        : img.image_file.url if img else None,
             'start_date' : start_date.strftime("%d %b %Y") if start_date else None,
             'end_date'   : end_date.strftime("%d %b %Y") if end_date else None,

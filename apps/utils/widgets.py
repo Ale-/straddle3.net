b2f5c9f20a325096dcccc4ea52e94ab65fed0f59
ale <a@414c45.net>
2018-05-22 13:53:26 +0200

    fix: Update apps/utils AdminImageWidget
    Solve 'seek of closed file' exception result of calling to a non-setted
    value of the ImageFieldFile

--- apps/utils/widgets.py
+++ apps/utils/widgets.py
@@ -7,11 +7,12 @@ from django.template.loader import render_to_string
 class AdminImageWidget(AdminFileWidget):
     def render(self, name, value, attrs=None):
         parent_widget = super(AdminImageWidget, self).render(name, value, attrs)
-        c = { 
+        print(value.__class__)
+        c = {
             'parent_widget': parent_widget,
             'url'          : value.url,
         } if value and 'url' in value else {
             'parent_widget': parent_widget,
-            'url'          : value,
-        }  
+            'url'          : None,
+        }
         return render_to_string("admin-image-widget.html", c)
